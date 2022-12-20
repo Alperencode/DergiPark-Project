5673508f815947228994cb483bf466cc46916b07
Alperen Aga <alperencode@gmail.com>
2022-12-20 23:43:54 +0300

    Major Update Add new main file & src directory

--- main.py
+++ main.py
@@ -1,201 +1,48 @@
-from bs4 import BeautifulSoup
-import time,requests,os,json
+"""
+- DergiPark's information
+    - DergiPark has 100+ pages
+    - Each page has 24 Magazine
+    - Each Magazine has average 10 Articles
+    - Total Articles: 24000+
 
-data_list = []
-parsingCounter = 0
-errorCounter = 0
+- Roadmap of the project
+    - Get the current last page number
+    - Create a link for each page
+    - Get the magazine links from each page
+    - Get the article links from each magazine
+    - Parse the data from each article
 
-# --------- Loading Animation/Message ---------
-loadingCounter = 0
-animation = "|/-\\"
-def loadingAnimation(text):
-    global loadingCounter,animation
-    loadingCounter += 1
-    print(f"{text} {animation[loadingCounter % len(animation)]}", end="\r")
-# --------- Loading Animation/Message End ---------
+@Author: Alperen Ağa
+@Date: 02.04.2022 (dd.mm.yyyy)
+@Last Update: 19.12.2022 (dd.mm.yyyy)
+"""
 
-# --------- Gathering Links ---------
-linkList = []
-magazine_links = []
+from src import *
 
-# Parsing first page to get last page number
-url = "https://dergipark.org.tr/tr/search?q=&section=journal"
-url = requests.get(url)
-soup = BeautifulSoup(url.content,"lxml")
+# Starting timer
+start_time = datetime.datetime.now()
 
-# Gathering last page number 
-page_slider = soup.find("ul",class_="kt-pagination__links mx-auto")
-last_page = int(page_slider.find_all("li")[-2].text)
-del url
+# Creating output directories
+MakeDir("OutputTXT")
+MakeDir("OutputJSONL")
 
-# Generating links for all pages
-def get_pages(pageNumber):
-    url = f"https://dergipark.org.tr/tr/search/{pageNumber}?q=&section=journal"
-    linkList.append(url)
+# Parsing pages and creating page links
+for page_number in range(1, last_page+1):
+    GetPages(page_number)
 
-# Gathering each magazine link
-def get_magazine_links(url):
-    url = requests.get(url)
-    soup = BeautifulSoup(url.content,"lxml")
-    # finding all magazines
-    magazines = soup.find_all("h5",class_="card-title")
-    for link in magazines:
-        magazine_links.append(link.a.get('href'))
+# Gathering magazine links from each page
+for url in link_list:
+    GetMagazineLinks(url)
 
-# range loop for use get_pages function
-for pageNumber in range(1,last_page+1):
-    get_pages(pageNumber)
-
-# using get_magazine_links function
-for url in linkList:
-    loadingAnimation("Gathering magazine links from each page")
-    get_magazine_links(url)
 print("\nAll magazines links gathered")
-# --------- Gathering Links End ---------
-
-# --------- Parsing the Data --------- 
-# Parsing the data from each article link
 print("\nStarting parsing the data")
-def ParseArticle(articleLink):
-    global parsingCounter
-    parsingCounter += 1
-    
-    # 429 means that the server is overloaded
-    url = requests.get(articleLink)
-    if url.status_code == 429:
-        time.sleep(int(url.headers["Retry-After"]) + 10)
-        print(f"waited {int(url.headers['Retry-After']) + 10} seconds")   
-    
-    soup = BeautifulSoup(url.content,"lxml")
-    
-    # creating new dictionary for each article
-    dataDict = {}
 
-    # Makale Başlığı
-    article_title = soup.find("h3",class_="article-title").text.strip()
-    if len(article_title) < 1:
-        article_title = soup.findAll("h3",class_="article-title")[1].text.strip()
-    
-    # Özet 
-    ozet_section = soup.find("div",class_="article-abstract data-section")
-    dataDict['Özet'] = ozet_section.text.replace("\n","")
-    
-    # Konu
-    # split the values due ','
-    info_table = soup.find("table",class_="record_properties table")
-    tr_tags = info_table.find_all("tr")
-    try:
-        for tr_tag in tr_tags:
-            if tr_tag.th.text.strip() == "Konular":
-                konu_list = []
-                for konu in tr_tag.td.text.strip().split(","):
-                    konu_list.append(konu.strip())
-                dataDict['Konular'] = konu_list
-    except:
-        dataDict['Konular'] = ''
+# Parsing each magazine and each article in these magazines
+for magazine_link in magazine_links:
+    ParseMagazine(magazine_link)
 
-    # Yazar İsimleri
-    article_authors = soup.find("p",class_="article-authors")
-    try:
-        names = []
-        author_names = article_authors.find_all("a",class_="is-user")
-        for name in author_names:
-            names.append(name.text.strip())
-        dataDict['Yazar İsimleri'] = names
-    except:
-        author_name = article_authors.find("a").text.strip()
-        dataDict['Yazar İsimleri'] = author_name
-
-    # Yayın Yılı
-    # Note: Because of the 'Yayın yılı' is not specified in any class, I gathered the table which has the 'Yayın yılı' data
-    # Then I made a control to find the 'Yayın yılı'
-    info_table = soup.find("table",class_="record_properties table")
-    tr_tags = info_table.find_all("tr")
-    for tr_tag in tr_tags:
-        if tr_tag.th.text.strip() == "Yayımlanma Tarihi":
-            dataDict['Yayın Yılı'] = tr_tag.td.text.strip()
-
-    # Dergi İsmi
-    magazine_name = soup.find("h1",attrs={"id":"journal-title"}).text.strip()
-    dataDict['Dergi İsmi'] = magazine_name
-
-    # Yayın Sayfa URL
-    dataDict['Yayın Sayfa URL'] = articleLink
-
-    # Yayın PDF'i
-    tool_bar = soup.find("div", attrs={"id":"article-toolbar"})
-    a_tags = tool_bar.find_all("a")
-    shortLink = a_tags[0].get("href")
-    pdf_link = f"https://dergipark.org.tr{shortLink}"
-    dataDict['Yayın PDF'] = pdf_link
-    
-    # I'm adding Parsed data to the data_list
-    data_list.append(dataDict)
-
-    print(f"{parsingCounter}. Article created")
-
-    # OUTPUT TO TXT FILE
-    # Writing the information into txt file (Used in articles directory)
-    # But its not necessary for the main project
-    # ↓ Uncomment the code below to use
-    # OutputToTXTFile(dataDict)
-
-# You can add specific Controls for articles
-def AddControl(label):
-    labelText = label.text.split(".")
-    labelText.pop(0)
-    labelText = ' '.join(labelText)
-    labelText = labelText.replace("\n","").lower()
-    # Example return for labelText: "COVID-19, Wine Routes, Crisis Management and Resilience Amongst Rural Wine Tourism Businesses"
-
-    # Example of usage:
-    # if "COVID-19" in labelText:
-    #     return True
-    # else:
-    #     return False
-
-# Parsing the magazine to get each article
-def ParseMagazine(magazineLink):
-    global errorCounter
-    url = requests.get(magazineLink)
-    if url.status_code == 429:
-        time.sleep(int(url.headers["Retry-After"]) + 10)
-        print(f"slept {int(url.headers['Retry-After']) + 10} seconds")   
-    soup = BeautifulSoup(url.content,"lxml")
-    labels = soup.find_all("a",class_="card-title article-title")
-    for label in labels:
-        # AddControl(label)
-        try:
-            ParseArticle(f"https:{label.get('href')}")
-        except:
-            try:
-                ParseArticle(f"{label.get('href')}")
-            except:
-                errorCounter += 1
-                print(f"An error occurred [{errorCounter}. Error] , url: {label.get('href')}")
-
-for magazineLink in magazine_links:
-    ParseMagazine(magazineLink)
-# --------- Parsing the Data End ---------
-
-# --------- Output of the Data ---------
-def OutputToJSONLFile():
-    with open('article.jsonl', 'w',encoding='utf-8') as outfile:
-        for entry in data_list:
-            json.dump(entry, outfile,ensure_ascii=False)
-            outfile.write('\n')
 OutputToJSONLFile()
+OutputToTXTFile()
 
-def OutputToTXTFile(data):
-    global parsingCounter
-    with open(f"article{parsingCounter}.txt",'w',encoding='utf-8') as txt:
-        txt.write(f"Makale Başlığı: {data['Makale Başlığı']}\n")
-        txt.write(f"Özet: {data['Özet']}\n")
-        txt.write(f"Yazar isimleri: {data['Yazar İsimleri']}\n")
-        txt.write(f"Yayın Yılı: {data['Yayın Yılı']}\n")
-        txt.write(f"Dergi ismi: {data['Dergi İsmi']}\n")
-        txt.write(f"Yayın sayfa url: {data['Yayın Sayfa URL']}\n")
-        txt.write(f"Yayın pdf linki: {data['Yayın PDF']}\n")
-# --------- Output of the Data End ---------
-
-print(f"\nFinished with {errorCounter} errors.")
+finish_time = datetime.datetime.now() - start_time
+Stats(finish_time)    
---
+++ src/methods.py
@@ -0,0 +1,253 @@
+from bs4 import BeautifulSoup
+import requests,os,json
+
+data_list, link_list, magazine_links = [], [], []
+parsed_counter, error_counter, loading_counter, request_counter, txt_counter, session_counter = 0, 0, 0, 0, 0, 1
+MAX_REQUEST = 100
+
+session = requests.Session()
+
+def SendRequest(url):
+    """
+    Sends a request to the given url
+    """
+    global request_counter, error_counter
+    request_counter += 1
+
+    if request_counter > MAX_REQUEST:
+        ChangeSession()
+        request_counter = 0
+
+    try:
+        url = session.get(url)
+    except Exception as e:
+        error_counter += 1
+        return
+
+    return url
+
+def ChangeSession():
+    """
+    Changes the session to avoid connection errors
+    [Eg: 429 - Too Many Requests]
+    """
+    global session, session_counter
+    session_counter += 1
+    session = requests.Session()
+
+def GetLastPage():
+    """
+    Gets the last page number
+    """
+    url = SendRequest("https://dergipark.org.tr/tr/search?q=&section=journal")
+    soup = BeautifulSoup(url.content,"lxml")
+
+    # Gathering last page number 
+    page_slider = soup.find("ul",class_="kt-pagination__links mx-auto")
+    last_page = int(page_slider.find_all("li")[-2].text)
+
+    return last_page
+
+last_page = GetLastPage()
+
+def LoadingAnimation(text):
+    """
+    Creates Loading Animation with argument string
+    """
+    global loading_counter
+    animation = "|/-\\"
+    loading_counter += 1
+    print(f"{text} {animation[loading_counter % len(animation)]}", end="\r")
+
+def GetPages(page_number):
+    """
+    Generates links for all pages
+    """
+    url = f"https://dergipark.org.tr/tr/search/{page_number}?q=&section=journal"
+    link_list.append(url)
+
+def GetMagazineLinks(url_str):
+    """
+    Gathering each magazine link from each page
+    """
+    LoadingAnimation(f"Gathering magazines [Approximately {last_page*24}]")
+    url = SendRequest(url_str)
+
+    soup = BeautifulSoup(url.content,"lxml")
+
+    magazines = soup.find_all("h5",class_="card-title")
+    for link in magazines:
+        magazine_links.append(link.a.get('href'))
+
+
+def ParseArticle(articleLink):
+    """
+    Parsing each article data in 8 main headings
+    """
+    global parsed_counter, error_counter
+    
+    url = SendRequest(articleLink)
+    if url == None:
+        error_counter += 1
+        return
+    soup = BeautifulSoup(url.content,"lxml")
+
+    # Creating a dictionary to store the data
+    data_dict = {}
+
+    # 1) Makale Başlığı (Article Title)
+    article_title = soup.find("h3",class_="article-title").text.strip()
+    if len(article_title) < 1:
+        article_title = soup.findAll("h3",class_="article-title")[1].text.strip()
+    data_dict['Makale Başlığı'] = article_title.replace("\n","").replace("  ","").replace("\t","")
+    
+    # 2) Özet 
+    ozet_section = soup.find("div",class_="article-abstract data-section")
+    data_dict['Özet'] = ozet_section.text.replace("\n","")[2:] if ozet_section.text[0:2] != "Öz" else ozet_section.text.replace("\n","")[2:]
+    
+    # 3) Konular (Topics)
+    # split the values due ','
+    info_table = soup.find("table",class_="record_properties table")
+    tr_tags = info_table.find_all("tr")
+    try:
+        for tr_tag in tr_tags:
+            if tr_tag.th.text.strip() == "Konular":
+                konu_list = []
+                for konu in tr_tag.td.text.strip().split(","):
+                    konu_list.append(konu.strip())
+                data_dict['Konular'] = konu_list
+    except:
+        data_dict['Konular'] = ''
+
+    # 4) Yazar İsimleri (Author Names)
+    article_authors = soup.find("p",class_="article-authors")
+    try:
+        names = []
+        author_names = article_authors.find_all("a",class_="is-user")
+        for name in author_names:
+            names.append(name.text.strip())
+        data_dict['Yazar İsimleri'] = names
+    except:
+        author_name = article_authors.find("a").text.strip()
+        data_dict['Yazar İsimleri'] = author_name
+
+    # 5) Yayın Yılı (Publication Year)
+    # Scraping the publication year from the table
+    info_table = soup.find("table",class_="record_properties table")
+    tr_tags = info_table.find_all("tr")
+    for tr_tag in tr_tags:
+        if tr_tag.th.text.strip() == "Yayımlanma Tarihi":
+            data_dict['Yayın Yılı'] = tr_tag.td.text.strip()
+
+    # 6) Dergi İsmi (Magazine Name)
+    magazine_name = soup.find("h1",attrs={"id":"journal-title"}).text.strip()
+    data_dict['Dergi İsmi'] = magazine_name
+
+    # 7) Yayın Sayfa URL (Article Page URL)
+    data_dict['Yayın Sayfa URL'] = articleLink
+
+    # 8) Yayın PDF (Article PDF)
+    tool_bar = soup.find("div", attrs={"id":"article-toolbar"})
+    a_tags = tool_bar.find_all("a")
+    short_link = a_tags[0].get("href")
+    pdf_link = f"https://dergipark.org.tr{short_link}"
+    data_dict['Yayın PDF'] = pdf_link
+    
+    # Appending the data to the data_list
+    data_list.append(data_dict)
+
+    parsed_counter += 1
+    LoadingAnimation(f"Parsed Articles: {parsed_counter}, Errors: {error_counter} [Session count: {session_counter}]")
+
+
+def ParseMagazine(magazineLink):
+    """
+    Parsing the magazine link to get each article
+    """
+    global error_counter
+    url = SendRequest(magazineLink)
+
+    if url == None:
+        error_counter += 1
+        return
+
+    soup = BeautifulSoup(url.content,"lxml")
+    labels = soup.find_all("a",class_="card-title article-title")
+
+    for label in labels:
+        # if AddControl('Covid'):
+        try:
+            ParseArticle(f"https:{label.get('href')}")
+        except:
+            try:
+                ParseArticle(label.get('href'))
+            except:
+                error_counter += 1
+
+def AddControl(label):
+    """
+    Adding control to the article label (Eg: 'Covid')
+    Can be used to filter the articles
+    Returns boolean
+    """
+    label_text = label.split(".")
+    label_text.pop(0)
+    label_text = ' '.join(label_text)
+    label_text = label_text.replace("\n","").lower()
+
+    if label.lower() in label_text:
+        return True
+    return False
+
+    # Example usage:
+    # if AddControl('Covid'):
+    #     ParseArticle(f"https:{label.get('href')}")
+
+
+def MakeDir(dir_name):
+    """
+    Creating a directory
+    If the directory exists, it will be deleted
+    """
+    import os, shutil
+    if os.path.exists(dir_name):
+        shutil.rmtree(dir_name)
+    os.mkdir(dir_name)
+
+def OutputToJSONLFile():
+    """
+    Outputting the data to a JSONL file
+    """
+    if not data_list:
+        return
+    with open('OutputJSONL/articles.jsonl', 'w',encoding='utf-8') as outfile:
+        for entry in data_list:
+            json.dump(entry, outfile,ensure_ascii=False)
+            outfile.write('\n')
+
+def OutputToTXTFile():
+    """
+    Outputting the data to a TXT file
+    """
+    if not data_list:
+        return
+    for counter in range(0, parsed_counter):
+        with open(f"OutputTXT/articles_{counter+1}.txt", "w",encoding='utf-8') as file:
+            for key, value in data_list[counter].items():
+                if type(value) == list:
+                    file.write(f"{key}: ")
+                    for item in value:
+                        file.write(f"{item}, ") if item != value[-1] else file.write(f"{item}")
+                file.write(f"{key}: {value}\n")
+    
+def Stats(time):
+    """
+    Printing the stats of the program
+    """
+    print(f"\n\nTotal time: {time}")
+    if parsed_counter == 0:
+        print("No articles parsed")
+        return
+    print(f"Total articles: {parsed_counter} [Succes rate: %.2f%%]" % (parsed_counter/(parsed_counter+error_counter)*100))
+    print(f"Total errors: {error_counter} [Error rate: %.2f%%]" % (error_counter/(parsed_counter+error_counter)*100))
+    print(f"Session count: {session_counter}")
